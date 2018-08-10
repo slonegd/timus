2fa204a7bd1be9d16da3b84901aa10a4cb936b5e
slonegd <slonkdv@mail.ru>
2018-08-10 06:38:18 +0300

    start 2002

---
+++ 2002.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+int main()
+{
+    std::map<std::string,std::string> users;
+    std::vector<>
+    std::size_t n;
+    std::cin >> n;
+    std::vector<std::string> res;
+    res.reserve(n);
+    while (n--) {
+        std::string line;
+        std::cin.getline (line);
+        std:vector<std::string> split;
+        std::istringstream
+    }
+}
